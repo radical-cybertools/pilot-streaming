53ae826f444f3c951aba7cf0fc6c429ac3b35c3a
Andre Luckow <andre.luckow@gmail.com>
2017-12-16 14:39:18 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Pilot-Streaming
+
+Last Updated: 12/09/2017
+
+# Overview:
+
+Use [SAGA](http://saga-project.github.io/saga-python/) to manage Streaming environment 
+ consisting of Kafka, Spark Streaming, Flink and Dask on HPC systems. 
+ It is based on the [SAGA-Hadoop](http://github.com/drelu/saga-hadoop) tool and extends 
+ it for streaming.
+
+Currently supported SAGA adaptors:
+
+- Fork
+- Torque
+
+Requirements:
+
+	* PBS/Torque cluster
+	* Working directory should be on a shared filesystem
+	* Setup password-less documentation
+	* JAVA needs to be installed and in PATH
+
+
+
+
+# Usage
+
+Requirement (in case a manual installation is required):
+    
+    pip install pykafka saga-python
+
+Try to run a local Hadoop (e.g. for development and testing)
+	
+	
+    pip install saga-hadoop
+    saga-hadoop --resource fork://localhost
+    
+    
+Try to run a Hadoop cluster inside a PBS/Torque job:
+
+    saga-hadoop --resource pbs+ssh://india.futuregrid.org --number_cores 8
+
+Some Blog Posts about SAGA-Hadoop:
+
+    * <http://randomlydistributed.blogspot.com/2011/01/running-hadoop-10-on-distributed.html>
+
+
+# Packages:
+
+see `hadoop1` for setting up a Hadoop 1.x.x cluster
+
+see `hadoop2` for setting up a Hadoop 2.7.x cluster
+ 
+see `spark` for setting up a Spark 2.2.x cluster
+
+see `kafka` for setting up a Kafka 1.0.x cluster
+
+see `flink` for setting up a Flink 1.1.4 cluster
+
+see `dask` for setting up a Dask Distributed 1.20.2 cluster
+
+
+# Examples:
+
+
+***Stampede:***
+
+    saga-hadoop --resource=slurm://localhost --queue=normal --walltime=239 --number_cores=256 --project=xxx
+
+
+***Gordon:***
+
+    saga-hadoop --resource=pbs://localhost --walltime=59 --number_cores=16 --project=TG-CCR140028 --framework=spark
+    
+
+***Wrangler***
+
+    export JAVA_HOME=/usr/java/jdk1.8.0_45/
+    saga-hadoop --resource=slurm://localhost --queue=normal --walltime=59 --number_cores=24 --project=xxx
+
+
---
+++ setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+import subprocess
+
+try:
+    import saga
+except:
+    print "Installing BigJob and SAGA/Python."
+
+if sys.version_info < (2, 6):
+    sys.stderr.write("BigJob requires Python 2.6 and above. Installation unsuccessful!")
+    sys.exit(1)
+
+VERSION_FILE="VERSION"    
+    
+
+def update_version():
+    if not os.path.isdir(".git"):
+        print "This does not appear to be a Git repository."
+        return
+    try:
+        p = subprocess.Popen(["git", "describe",
+                              "--tags", "--always"],
+                             stdout=subprocess.PIPE)
+    except EnvironmentError:
+        print "Unable to run git, not modifying VERSION"
+        return
+    stdout = p.communicate()[0]
+    if p.returncode != 0:
+        print "Unable to run git, not modifying VERSION"
+        return
+    
+    ver = stdout.strip()
+    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'VERSION')
+    f = open(fn, "w")
+    f.write(ver)
+    f.close()
+    print "SAGA-Hadoop VERSION: '%s'" % ver
+
+
+def get_version():
+    try:
+        fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'VERSION')
+        f = open(fn)
+        version = f.read().strip()
+        f.close()
+    except EnvironmentError:
+        return "-1"
+    return version
+
+def create_manifest():
+    pass
+
+    
+#update_version()
+    
+setup(name='Pilot-Streaming',
+      version=get_version(),
+      description='Framework for Management of Streaming Frameworks on HPC clusters (Torque/PBS/SLURM)',
+      author='Andre Luckow',
+      author_email='aluckow@clemson.edu',
+      url='https://github.com/radical-cybertools/pilot-streaming',
+      classifiers = ['Development Status :: 5 - Production/Stable',                  
+                    'Programming Language :: Python',
+                    'Environment :: Console',                    
+                    'Topic :: Utilities',
+                    ],
+      platforms = ('Unix', 'Linux', 'Mac OS'),
+      license = "License :: OSI Approved :: Apache Software License",
+      include_package_data = True,
+      package_dir = {'':'.'},
+      packages=find_packages(),
+            # ['hadoop1', 'hadoop2', 'hadoop2.configs.default',
+            #     'hadoop2.configs.default',
+            #     'hadoop2.configs.stampede',
+            #     'hadoop2.configs.gordon',
+            #     "spark", "commandline"],
+
+      # data files for easy_install
+      package_data= {'': ['*.xml', '*.yaml', '*.properties']},
+      install_requires=['uuid', 'radical.utils', 'saga-python', 'pykafka', 'argparse', "python-hostlist", "setuptools-git" ],
+      entry_points = {
+        'console_scripts': ['psm=commandline.main:main',
+                            'pilot-streaming-manager=commandline.main:main']
+      }
+)
